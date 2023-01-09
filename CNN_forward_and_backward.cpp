35b613875d70f5758d497e79ec9295342ae4211b
Qinwu Xu <78186650+qinwuxutexas@users.noreply.github.com>
2023-01-09 10:22:52 -0500

    Update CNN_forward_and_backward.cpp

--- CNN_forward_and_backward.cpp
+++ CNN_forward_and_backward.cpp
@@ -6,8 +6,11 @@ Notes:
 1) the methodology is referred to Andrew NG., Deep Learning Specilization, Coursera, which provided the backbone and framework of incomplete Python code.
 2) the major contribution of this prototype convers:
   a) it complete the full procedure of CNN for classification and object detection, including the additonal layers of flatten-> fully-connected layers -> classification -> location detection, and data in and results out with the main function; 
-  b) it wrotes in C++ and organized in object oriented and with parallism option for CPUs.
-
+  b) it wrotes in C++ and with parallism option for CPUs. It is designed in object oriented with three classes and one main function: 
+     (1) class: math operation; 
+     (2) class: forward computation;
+     (3) Class: backward computation;
+     (4) main: read data in, train the model and output prediction results.
 */
 
 #include <iostream>
